73fc51797ae676dd67c3b928c379e4f1b10b9af7
Michele Scuttari <mscuttari@users.noreply.github.com>
2019-07-19 15:21:34 +0200

    Copyright notice

--- miosix-kernel/src/fft/window.h
+++ miosix-kernel/src/fft/window.h
@@ -1,18 +1,37 @@
-#ifndef NEURALNETWORK_WINDOW_H
-#define NEURALNETWORK_WINDOW_H
+/**************************************************************************
+ * Copyright (C) 2019 Michele Scuttari, Marina Nikolic                    *
+ *                                                                        *
+ * This program is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or      *
+ * (at your option) any later version.                                    *
+ *                                                                        *
+ * This program is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ * GNU General Public License for more details.                           *
+ *                                                                        *
+ * You should have received a copy of the GNU General Public License      *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ **************************************************************************/
+
+#ifndef WINDOW_H
+#define WINDOW_H
 
 #include <cstdio>
-#include <cstdlib>
-#include <math.h>
 
 class WindowFunction {
 public:
     explicit WindowFunction(size_t size) : size(size) {};
 
-    /** Get the window size */
+    /**
+     * Get the window size
+     */
     size_t getSize();
 
-    /** Apply the window function to a value given its index */
+    /**
+     * Apply the window function to a value given its index
+     */
     virtual float apply(float value, int index) = 0;
 
 protected:
@@ -30,4 +49,4 @@ private:
     double* multipliers;
 };
 
-#endif //NEURALNETWORK_WINDOW_H
+#endif /* WINDOW_H */
